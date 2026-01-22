f612c15dcd3fdb7ea4f917e49c33306b97849a45
Mayur Kumavat <mkmayur4480@gmail.com>
2026-01-22 12:34:39 +0530

    carousol added

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,11 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './redux/Store.js'
 import { BrowserRouter } from 'react-router-dom'
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+          <App />
       </Provider>
     </BrowserRouter>
   </StrictMode>,
