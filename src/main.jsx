6abbae25e67141d0f4c5bc6d7a3a1a81f2a00b38
Mayur Kumavat <mkmayur4480@gmail.com>
2026-01-22 14:02:45 +0530

    seo

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,12 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './redux/Store.js'
 import { BrowserRouter } from 'react-router-dom'
-import { HelmetProvider } from "react-helmet-async";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </BrowserRouter>
-    </HelmetProvider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>,
 )
